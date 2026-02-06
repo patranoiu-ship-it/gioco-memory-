f53058cccc42553cd8358e09d9a792e00335f1a3
patranoiu-ship-it <patranoiu.alexandrugabriel.studente@itispaleocapa.it>
2026-02-06 11:56:07 +0100

    Add files via upload
    aggiornamento 06_02

---
+++ memory_25.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+
+
+int main(int argc, char** argv) {
+	srand(time(NULL));
+   
+   int l, val;          // valore ASCII
+   int cart=rand()%4+4;
+   char x[7][7];
+   char vis[7][7];
+   cout << "la matrice ha una dimensione: "<<cart<<"x"<<cart<<endl;
+    
+    //per nascondere i simboli
+    for(int i=0;i<cart;i++)
+    {
+        for(int j=0;j<cart;j++)
+        {
+            vis[i][j]='*';
+        }
+	}    
+   
+   //creare le coppie
+   char v[49]; //dichiarato v a 49 perchè cosi anche nel caso massimo tutti i simboli sono contenuti
+    int k=0;
+    for(int i=0;i<(cart*cart)/2;i++) //serve per creare il numero di coppie
+    {  
+	    char c=rand()%26+65;
+        v[k]=c;
+        k=k+1;
+        v[k]=c;
+        k=k+1;
+    }									
+   
+   if(cart%2==1)
+   {
+   	cout<<0<<" ";
+   }
+   
+   //mischiare 
+   for(int i=0; i<cart*cart; i++)
+   {
+   	int s=rand()%(cart*cart);
+   	char temp=v[i];
+   	v[i]=v[s];
+   	v[s]=temp;
+   }
+   
+   k=0
+   for(int i=0; i<cart; i++)
+   {
+   	for(int j=0; j<cart; j++)
+   	{
+   		
+    }
+   }
+   
+   
+   
+   for(int i=0; i<cart; i++)
+   {
+   	for(int j=0; j<cart; j++)
+   	{
+   	cout<<vis[i][j]<<" ";
+	}
+	cout<<endl;
+   }
+   
+   
+   
+   
+   	
+	return 0;
+}
